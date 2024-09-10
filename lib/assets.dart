e612a6f2845f1bd6101a121c4449cc6cb58258fc
Hossein HassanNejad <hosseinspell@gmail.com>
2024-09-10 10:16:35 +0330

    optimise images

--- lib/assets.dart
+++ lib/assets.dart
@@ -1,14 +1,14 @@
 class Assets {
-  static String logo = 'assets/images/logo.png';
-  static String convert = 'assets/images/convert.png';
-  static String cafebazaarDL = 'assets/images/cafebazaar_download.png';
-  static String directDL = 'assets/images/direct_download.png';
-  static String casualUser = 'assets/images/casual_user.png';
-  static String techEnthusiasts = 'assets/images/tech_enthusiasts.png';
-  static String contentCreator = 'assets/images/content_creator.png';
-  static String professional = 'assets/images/professional.png';
-  static String chooseMedia = 'assets/images/choose_media.jpg';
-  static String chooseFormat = 'assets/images/choose_format.jpg';
-  static String convertMedia = 'assets/images/convert_media.jpg';
-  static String menu = 'assets/images/menu.png';
+  static String logo = 'assets/images/logo.webp';
+  static String convert = 'assets/images/convert.webp';
+  static String cafebazaarDL = 'assets/images/cafebazaar_download.webp';
+  static String directDL = 'assets/images/direct_download.webp';
+  static String casualUser = 'assets/images/casual_user.webp';
+  static String techEnthusiasts = 'assets/images/tech_enthusiasts.webp';
+  static String contentCreator = 'assets/images/content_creator.webp';
+  static String professional = 'assets/images/professional.webp';
+  static String chooseMedia = 'assets/images/choose_media.webp';
+  static String chooseFormat = 'assets/images/choose_format.webp';
+  static String convertMedia = 'assets/images/convert_media.webp';
+  static String menu = 'assets/images/menu.webp';
 }
